2797715cbd29a167a9882936475126f72a031613
Mariusz Zając <zajac@gmx.us>
2023-06-02 20:15:57 +0200

    Add TODO plan for few days

---
+++ CodingChallenge/LogoView.swift
@@ -0,0 +1,21 @@
+//
+//  LogoView.swift
+//  CodingChallenge
+//
+//  Created by Mariusz Zając on 02/06/2023.
+//
+
+import SwiftUI
+
+struct LogoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    //TODO: create logo
+    }
+}
+
+struct LogoView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogoView()
+    }
+}
